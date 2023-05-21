2976e5587bc5469419b5969560141953eaa0528f
wuweitan94 <116598776+wuweitan94@users.noreply.github.com>
2023-05-20 21:49:50 -0500

    run_pathogenic_fewshot_learning_test.sh

--- ncVar_effect_prediction/run_pathogenic_fewshot_learning_test.sh
+++ ncVar_effect_prediction/run_pathogenic_fewshot_learning_test.sh
@@ -11,8 +11,8 @@ do
 			cp template_gpu.slurm run.slurm
 			echo "python cnn_mlp_diff_pathogenic_test.py --structure_name ${hic} --model_input_path ../trained_model/fewshot_pathogenic_model/CNN_related_pathogenic/CNN_MLP_diff_pathogenic_${hic}_replicate${replicate}_fewshotsize${fewshotsize}.pkl --result_output_path model_prediction/CNN_MLP_diff_pathogenic_${hic}_replicate${replicate}_fewshotsize${fewshotsize}_auroc_auprc.npy" >> run.slurm
 			echo "python cnn_rnn_gcn_diff_pathogenic_test.py --structure_name ${hic} --model_input_path ../trained_model/fewshot_pathogenic_model/CNN_RNN_related_pathogenic/CNN_RNN_GCN_DNABERT_diff_pathogenic_${hic}_replicate${replicate}_fewshotsize${fewshotsize}.pkl --result_output_path model_prediction/CNN_RNN_GCN_DNABERT_diff_pathogenic_${hic}_replicate${replicate}_fewshotsize${fewshotsize}_auroc_auprc.npy" >> run.slurm
-			sbatch run_${hic}.slurm
-			rm run_${hic}.slurm
+			sbatch run.slurm
+			rm run.slurm
 		done
 	done
 done
